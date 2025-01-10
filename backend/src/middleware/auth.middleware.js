7c72db6bf631e6255ab90b2dbf46fdeb6638327f
Salman Rayeen <mohdsr242@gmail.com>
2025-01-10 12:53:17 +0530

    Testing routes.

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -4,29 +4,29 @@ import User from "../models/user.model.js";
 export const protectRoute = async (req, res, next) => {
 
     try {
-        const token = req.cookies.jwt;
+        // const token = req.cookies.jwt;
 
-        if (!token) {
-            return res.status(402).json({ message: "Unauthorized! - No Token found!" });
-        }
+        // if (!token) {
+        //     return res.status(401).json({ message: "Unauthorized! - No Token found!" });
+        // }
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        // const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        if (!decoded) {
-            return res.status(401).json({ message: "Unauthorized! - Invalid Token" });
-        }
+        // if (!decoded) {
+        //     return res.status(401).json({ message: "Unauthorized! - Invalid Token" });
+        // }
 
-        const user = await User.findById(decoded.userId).select("-password");
+        // const user = await User.findById(decoded.userId).select("-password");
 
-        if (!user) {
-            return res.status(401).json({ message: "Unauthorized! - User not found" });
-        }
+        // if (!user) {
+        //     return res.status(401).json({ message: "Unauthorized! - User not found" });
+        // }
 
-        req.user = user;
+        // req.user = user;
         next();
 
     } catch (error) {
         console.log("Error in protectRoute", error.message);
-        return res.status(500).json({ message: "Internal Server Error!" });
+        return res.status(500).json({ message: "Internal Server Error!:Error in protectRoute" });
     }
 }
