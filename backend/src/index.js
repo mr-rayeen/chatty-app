ea9308048d51c5016ff0cad363c2ad35224489ca
Salman Rayeen <mohdsr242@gmail.com>
2025-01-10 12:39:59 +0530

    Adding api in frontend url in backend.

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(
 	cors({
 		origin: [
 			"http://localhost:5173",
-			"https://chatty-app-eta.vercel.app/",
+			"https://chatty-app-eta.vercel.app/api",
 			process.env.FRONTEND,
 		],
 		credentials: true,
