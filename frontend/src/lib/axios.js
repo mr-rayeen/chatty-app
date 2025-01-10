e90be8d3c45df767ef0cb20c742ff29999587bb9
Salman Rayeen <mohdsr242@gmail.com>
2025-01-10 12:22:27 +0530

    Updated changed axios url vercel to render.

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,9 @@ export const axiosInstance = axios.create({
 	baseURL:
 		import.meta.env.MODE === "development"
 			? "http://localhost:5001/api"
-			: "https://chatty-app-backend-sandy.vercel.app/api",
-	withCredentials: true,
+			: "https://chatty-app-ijzm.onrender.com/api",
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
