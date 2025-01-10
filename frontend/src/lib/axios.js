f53c391f9ef2f203a40e260c2c81e2eb9eec0bc1
Salman Rayeen <mohdsr242@gmail.com>
2025-01-10 12:36:38 +0530

    Reverted back to vercel.

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
 	baseURL:
 		import.meta.env.MODE === "development"
 			? "http://localhost:5001/api"
-			: "https://chatty-app-backend-sandy.vercel.app/",
+			: "https://chatty-app-backend-sandy.vercel.app/api",
 	withCredentials: true,
 	headers: {
 		"Content-Type": "application/json",
